e045747c007691e9bbb037c2d6416d3a44d577e3
Seth Goodman <smgoodman@vx05.sciclone.wm.edu>
2021-07-30 18:32:16 -0400

    Update input data

--- tuff_osm.py
+++ tuff_osm.py
@@ -44,11 +44,14 @@ id_field = "AidData Tuff Project ID"
 location_field = "Geographic Location"
 
 
-release_name = "test"
-input_csv_name = "tuff_osm_test.csv"
+# release_name = "test"
+# input_csv_name = "tuff_osm_test.csv"
 
-release_name = "2.0prerelease"
-input_csv_name = "ChineseOfficialFinance2.0_PreliminaryDataset_July222021.csv"
+# release_name = "2.0prerelease"
+# input_csv_name = "ChineseOfficialFinance2.0_PreliminaryDataset_July222021.csv"
+
+release_name = "prerelease_20210730"
+input_csv_name = "exportforseth.csv"
 
 # -----------------
 
