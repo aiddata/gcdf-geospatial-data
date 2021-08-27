113a14042070cec92456585522c6d1b5578b7576
Seth Goodman <sgoodman@aiddata.wm.edu>
2021-08-27 13:12:20 -0400

    Add support for concurrent.futures parallelization in addition to MPI

--- tuff_osm.py
+++ tuff_osm.py
@@ -526,15 +526,29 @@ def run_tasks(func, flist, parallel, max_workers=None, chunksize=1):
     wrapper_list = [(func, i) for i in flist]
     if parallel:
         # see: https://mpi4py.readthedocs.io/en/stable/mpi4py.futures.html
-        from mpi4py.futures import MPIPoolExecutor
+        # and: https://docs.python.org/3/library/concurrent.futures.html
+        try:
+            from mpi4py.futures import MPIPoolExecutor
+            mpi = True
+        except:
+            from concurrent.futures import ProcessPoolExecutor
+            mpi = False
         if max_workers is None:
-            if "OMPI_UNIVERSE_SIZE" not in os.environ:
-                raise ValueError("Mode set to parallel but max_workers not specified and OMPI_UNIVERSE_SIZE env var not found")
-            max_workers = os.environ["OMPI_UNIVERSE_SIZE"]
-            warnings.warn(f"Mode set to parallel but max_workers not specified. Defaulting to OMPI_UNIVERSE_SIZE env var value ({max_workers})")
-        with MPIPoolExecutor(max_workers=max_workers) as executor:
-            # results_gen = executor.starmap(func, flist, chunksize=chunksize)
-            results_gen = executor.starmap(_task_wrapper, wrapper_list, chunksize=chunksize)
+            if mpi:
+                if "OMPI_UNIVERSE_SIZE" not in os.environ:
+                    raise ValueError("Parallel set to True and mpi4py is installed but max_workers not specified and OMPI_UNIVERSE_SIZE env var not found")
+                max_workers = os.environ["OMPI_UNIVERSE_SIZE"]
+                warnings.warn(f"Parallel set to True (mpi4py is installed) but max_workers not specified. Defaulting to OMPI_UNIVERSE_SIZE env var value ({max_workers})")
+            else:
+                import multiprocessing
+                max_workers = multiprocessing.cpu_count()
+                warnings.warn(f"Parallel set to True (mpi4py is not installed) but max_workers not specified. Defaulting to CPU count ({max_workers})")
+        if mpi:
+            with MPIPoolExecutor(max_workers=max_workers) as executor:
+                results_gen = executor.starmap(_task_wrapper, wrapper_list, chunksize=chunksize)
+        else:
+            with ProcessPoolExecutor(max_workers=max_workers) as executor:
+                results_gen = executor.map(_task_wrapper, *zip(*wrapper_list), chunksize=chunksize)
         results = list(results_gen)
     else:
         results = []
