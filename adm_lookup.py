a06e183e7ac37d1950842c71541a393392783fa0
sgoodm <sgoodman@aiddata.wm.edu>
2023-07-19 16:04:04 -0400

    Drop adm units from merge that are below a specified intersection threshold

--- adm_lookup.py
+++ adm_lookup.py
@@ -5,10 +5,16 @@ from pathlib import Path
 
 import geopandas as gpd
 
-dataset_path = "/home/userx/Desktop/tuff_osm/output_data/3.0test20182020/results/2023_06_07_15_56/all_combined_global.geojson"
-value_field = "Amount.(Constant.USD2020)"
+
+dry_run = True
+intersection_threshold = 0.0001
+
+
+dataset_path = "/home/userx/Desktop/tuff_osm/output_data/3.0test2021/results/2023_07_11_16_53/all_combined_global.geojson"
+value_field = "Amount (Constant USD2021)"
 china_gdf = gpd.read_file(dataset_path)
 
+# china_gdf = china_gdf.loc[china_gdf.id ==97577]
 
 adm_data_dir = Path("/home/userx/Desktop/tuff_osm/gb_v5")
 adm_data_dir.mkdir(parents=True, exist_ok=True)
@@ -39,6 +45,12 @@ china_adm1_gdf.rename(columns={"adm1_id": "shapeID"}, inplace=True)
 china_adm1_gdf = china_adm1_gdf[["id", value_field, "geometry", "shapeID"]].copy()
 china_adm1_gdf = china_adm1_gdf.merge(adm1_gdf, on="shapeID", suffixes=("", "_adm1"))
 china_adm1_gdf["intersection_ratio"] = china_adm1_gdf["geometry"].intersection(china_adm1_gdf["geometry_adm1"]).area / china_adm1_gdf["geometry"].area
+
+initial_adm1_count = china_adm1_gdf.shape[0]
+china_adm1_gdf = china_adm1_gdf.loc[china_adm1_gdf["intersection_ratio"] > intersection_threshold].copy()
+final_adm1_count = china_adm1_gdf.shape[0]
+print(f"Removed {initial_adm1_count - final_adm1_count} adm1s (from {initial_adm1_count}) with intersection ratio < {intersection_threshold}. Final count is {final_adm1_count}.")
+
 china_adm1_gdf = china_adm1_gdf[["id", value_field, "shapeID", "shapeGroup", "shapeName", "geometry_adm1", "intersection_ratio"]].copy()
 china_adm1_gdf.rename(columns={"geometry_adm1": "geometry"}, inplace=True)
 china_adm1_gdf = gpd.GeoDataFrame(china_adm1_gdf, geometry="geometry", crs="EPSG:4326")
@@ -68,6 +80,12 @@ china_adm2_gdf.rename(columns={"adm2_id": "shapeID"}, inplace=True)
 china_adm2_gdf = china_adm2_gdf[["id", value_field, "geometry", "shapeID"]].copy()
 china_adm2_gdf = china_adm2_gdf.merge(adm2_gdf, on="shapeID", suffixes=("", "_adm2"))
 china_adm2_gdf["intersection_ratio"] = china_adm2_gdf["geometry"].intersection(china_adm2_gdf["geometry_adm2"]).area / china_adm2_gdf["geometry"].area
+
+initial_adm2_count = china_adm2_gdf.shape[0]
+china_adm2_gdf = china_adm2_gdf.loc[china_adm2_gdf["intersection_ratio"] > intersection_threshold].copy()
+final_adm2_count = china_adm2_gdf.shape[0]
+print(f"Removed {initial_adm2_count - final_adm2_count} adm2s (from {initial_adm2_count}) with intersection ratio < {intersection_threshold}. Final count is {final_adm2_count}.")
+
 china_adm2_gdf = china_adm2_gdf[["id", value_field, "shapeID", "shapeGroup", "shapeName", "geometry_adm2", "intersection_ratio"]].copy()
 china_adm2_gdf.rename(columns={"geometry_adm2": "geometry"}, inplace=True)
 china_adm2_gdf = gpd.GeoDataFrame(china_adm2_gdf, geometry="geometry", crs="EPSG:4326")
